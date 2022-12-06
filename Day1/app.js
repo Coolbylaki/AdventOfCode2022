67bebbeb4f169e11e4e548934b41075ac4e48f21
Lazar Stojanović <lakisd98@gmail.com>
2022-12-06 08:43:51 +0100

    Day 1 part 1 completed

---
+++ Day1/app.js
@@ -0,0 +1,25 @@
+// Space is separation from other elf inventory
+const fs = require("fs")
+// Calculate each elf calorie count in inventory and find the elf who is carrying most calories
+
+// PART 1
+function getCalories() {
+    fs.readFile("./input.txt", (err, data) => {
+        const calories = data.toString().split(/\r?\n/)
+        let elfCalories = 0
+        const allCalories = []
+        for (let calorie of calories) {
+            if (calorie === "") {
+                allCalories.push(elfCalories)
+                elfCalories = 0
+            } else {
+                elfCalories += parseInt(calorie)
+            }
+        }
+        const maxCalorie = Math.max(...allCalories)
+        console.log(maxCalorie)
+    })
+}
+getCalories()
+
+//PART 2
